a40a61b26f3319ddb5d4d6189ad5a4eb8d1d3bce
Marcelo Marques <marcelomarqueslima.mm@outlook.com>
2024-03-06 10:11:07 -0300

    separado classe de dados

---
+++ accounts.py
@@ -0,0 +1,12 @@
+account_list = {
+    '0001-02': {
+        'password': '12345',
+        'name': 'Fulano de Tal',
+        'value': 0
+    },
+    '0002-02': {
+        'password': '54321',
+        'name': 'Fulana de Tal',
+        'value': 0
+    }
+}
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import getpass
+import accounts
+
 print("*****************************************************")
 print("************ Caixa Eletrônico - MQS *****************")
 print("*****************************************************")
 account_typed = input("Insira sua Conta: ")
-password_typed = input("Insira sua Senha: ")
-print(account_typed)
-print(password_typed)
+password_typed = getpass.getpass('Digite sua senha: ')
+#print(account_typed)
+#print(password_typed)
 print("*****************************************************")
 print("*****************************************************")
+
+if account_typed in accounts.account_list and password_typed == accounts.account_list[account_typed]['password']:
+    print("Conta válida!")
+else:
+    print("Conta inválida!")
