d37132a7b05587cdd756996f788e8e11f0122e85
Marcelo Marques <marcelomarqueslima.mm@outlook.com>
2024-03-07 08:55:49 -0300

    Criação da inclusão de cedulas

--- accounts.py
+++ accounts.py
@@ -2,12 +2,20 @@ account_list = {
     '0001-02': {
         'password': '12345',
         'name': 'Marcelo Marques de Lima',
-        'value': 100
+        'value': 100,
+        'admin': False
     },
     '0002-02': {
         'password': '54321',
         'name': 'Elizabete Maciel Lauzimar',
-        'value': 50
+        'value': 50,
+        'admin': False
+    },
+    '1111-11': {
+        'password': '54321',
+        'name': 'Admin Banco',
+        'value': 500,
+        'admin': True
     }
 }
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import getpass
 import os
 import accounts
+import money
 
 while True:
     print(" ")
@@ -18,17 +19,27 @@ while True:
         print(" ")
         print("************* Menu de opções ************************")
         print("1 - Saldo")
+        if accounts.account_list[account_typed]['admin']:
+            print("10 - Incluir cédulas")
         print("*****************************************************")
         print(" ")
         name = accounts.account_list[account_typed]['name']
         print("Seja bem-vindo ao banco XPTO Sr(a) " + name)
         option_typed = input('Escolha uma das opções acima: ')
+        print(" ")
         if option_typed == '1':
             #print("Seu saldo é: " + accounts.account_list[account_typed]['value'])
             print("Seu saldo é: R$ %s" % accounts.account_list[account_typed]['value'])
+        elif option_typed == '10' and accounts.account_list[account_typed]['admin']:
+            amount_type = input("Digite a quantidade de cédulas: ")
+            money_bill_type = input("Digite o valor da tipo de cédulas: ")
+            #money.money_slips[money_bill_type] = money.money_slips[money_bill_type] + int(amount_type)
+            money.money_slips[money_bill_type] += int(amount_type)
+            print(money.money_slips)
     else:
         print("Conta inválida!")
 
+    print(" ")
     input('Precione <<ENTER>> para continuar')
 
     clear = 'cls' if os.name == 'nt' else 'clear'
