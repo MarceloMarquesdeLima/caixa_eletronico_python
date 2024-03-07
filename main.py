d472ee13eb7db13844442a6a8ba10bb4eba77fad
Marcelo Marques <marcelomarqueslima.mm@outlook.com>
2024-03-07 07:58:25 -0300

    Melhorias esteticas no layout

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import getpass
+import os
 import accounts
 
 while True:
+    print(" ")
     print("*****************************************************")
     print("************ Caixa Eletrônico - MQS *****************")
     print("*****************************************************")
     account_typed = input("Insira sua Conta: ")
     password_typed = getpass.getpass('Digite sua senha: ')
-    print("*****************************************************")
-    print("*****************************************************")
-    #print(account_typed)
-    #print(password_typed)
 
     if account_typed in accounts.account_list and password_typed == accounts.account_list[account_typed]['password']:
+        os.system('cls' if os.name == 'nt' else 'clear')
         print("*****************************************************")
         print("************ Caixa Eletrônico - MQS *****************")
         print("*****************************************************")
+        print(" ")
+        print("************* Menu de opções ************************")
         print("1 - Saldo")
+        print("*****************************************************")
+        print(" ")
+        name = accounts.account_list[account_typed]['name']
+        print("Seja bem-vindo ao banco XPTO Sr(a) " + name)
         option_typed = input('Escolha uma das opções acima: ')
         if option_typed == '1':
-            name = accounts.account_list[account_typed]['name']
             #print("Seu saldo é: " + accounts.account_list[account_typed]['value'])
-            print("Seu saldo "+ name +" é: %s" % accounts.account_list[account_typed]['value'])
+            print("Seu saldo é: R$ %s" % accounts.account_list[account_typed]['value'])
     else:
         print("Conta inválida!")
+
+    input('Precione <<ENTER>> para continuar')
+
+    clear = 'cls' if os.name == 'nt' else 'clear'
+    os.system(clear)
+
 
 
 
@@ -35,3 +45,9 @@ while True:
 #        print('Conta válida')
 #if not flag:
 #    print('Conta Inválida')
+
+    # Outra forma de limpar tela
+    #if os.name == 'nt': #Windows
+    #    os.system('cls')
+    #else:
+    #    os.system("clear")
