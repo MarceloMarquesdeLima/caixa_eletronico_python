65fad8937a9fc654157b5a02a3d902b010e23cd6
Marcelo Marques <marcelomarqueslima.mm@outlook.com>
2024-03-07 06:40:25 -0300

    Validação login e demonstração saldo com nome do cliente

--- accounts.py
+++ accounts.py
@@ -1,12 +1,29 @@
 account_list = {
     '0001-02': {
         'password': '12345',
+        'name': 'Marcelo Marques de Lima',
+        'value': 100
+    },
+    '0002-02': {
+        'password': '54321',
+        'name': 'Elizabete Maciel Lauzimar',
+        'value': 50
+    }
+}
+
+'''
+account_list = [
+     {
+        'agency': '0001-02',
+        'password': '12345',
         'name': 'Fulano de Tal',
         'value': 0
     },
-    '0002-02': {
+    {
+        'agency': '0002-02',
         'password': '54321',
         'name': 'Fulana de Tal',
         'value': 0
     }
-}
+]
+'''
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import getpass
 import accounts
 
-print("*****************************************************")
-print("************ Caixa Eletrônico - MQS *****************")
-print("*****************************************************")
-account_typed = input("Insira sua Conta: ")
-password_typed = getpass.getpass('Digite sua senha: ')
-#print(account_typed)
-#print(password_typed)
-print("*****************************************************")
-print("*****************************************************")
+while True:
+    print("*****************************************************")
+    print("************ Caixa Eletrônico - MQS *****************")
+    print("*****************************************************")
+    account_typed = input("Insira sua Conta: ")
+    password_typed = getpass.getpass('Digite sua senha: ')
+    print("*****************************************************")
+    print("*****************************************************")
+    #print(account_typed)
+    #print(password_typed)
 
-if account_typed in accounts.account_list and password_typed == accounts.account_list[account_typed]['password']:
-    print("Conta válida!")
-else:
-    print("Conta inválida!")
+    if account_typed in accounts.account_list and password_typed == accounts.account_list[account_typed]['password']:
+        print("*****************************************************")
+        print("************ Caixa Eletrônico - MQS *****************")
+        print("*****************************************************")
+        print("1 - Saldo")
+        option_typed = input('Escolha uma das opções acima: ')
+        if option_typed == '1':
+            name = accounts.account_list[account_typed]['name']
+            #print("Seu saldo é: " + accounts.account_list[account_typed]['value'])
+            print("Seu saldo "+ name +" é: %s" % accounts.account_list[account_typed]['value'])
+    else:
+        print("Conta inválida!")
+
+
+
+#Outra forma de fazer validação
+#flag = False
+#for account in accounts.account_list:
+#    if account_typed == account['agency'] and password_typed == account['password']:
+#        flag = True
+#        print('Conta válida')
+#if not flag:
+#    print('Conta Inválida')
